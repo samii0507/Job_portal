d58b8eb16ccf1e1d6fadbf45c5f2a9ce267209c8
Samitha <samitha.nillupul1440@gmail.com>
2024-05-15 16:28:45 +0530

    debug login

--- django_project/users/form.py
+++ django_project/users/form.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import get_user_model
 
-class RegisterForm(UserCreationForm):
+class RegisterUserForm(UserCreationForm):
     class Meta:
         model = get_user_model()
         fields = ['username', 'email', 'password1', 'password2']
--- django_project/users/urls.py
+++ django_project/users/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('register_applicant/', views.register_applicant, name='register_applicant'),
-    path('register_recruiter/', views.register_recruiter, name='register_recruiter'),
+    path('register_applicant/', views.register_applicant, name='register-applicant'),
+    path('register_recruiter/', views.register_recruiter, name='register-recruiter'),
     path('login/', views.login_user, name='login'),
     path('logout/', views.logout_user, name='logout'),
 ]
