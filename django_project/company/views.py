70db8a80cbbef498f832a7f770a66c529239272d
Samitha <samitha.nillupul1440@gmail.com>
2024-05-15 21:22:35 +0530

    company form

--- django_project/company/views.py
+++ django_project/company/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib import messages
+from .models import Company
+from users.models import User
+from .form import UpdateCompanyForm
 
-# Create your views here.
+
+def update_company(request):
+    company =Company.objects.get(user=request.user)
+    if request.method == 'POST':
+        form = UpdateCompanyForm(request.POST, instance=company)
+        if form.is_valid():
+            var = form.save(commit=False)
+            user = User.objects.get(id=request.user)
+            user.has_company = True
+            var.save()
+            user.save()
+            messages.info(request,'Your company is active now.You can start creating job ads')
+            return redirect('dashboard')
+        else:
+            messages.warning(request,'Something went wrong')
+    else:
+        form = UpdateCompanyForm(instance=company)
+        context = {
+            'form':form
+        }
+        return render(request,'company/update_company.html',context)
+
+#view company details
+ 
+def company_details(request,pk): 
+    company = Company.objects.get(pk=pk)
+    context = {
+        'company':company
+    }
+    return render(request,'company/company_details.html',context)
